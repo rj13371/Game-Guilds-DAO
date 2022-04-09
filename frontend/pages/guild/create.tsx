44bf4a1a18757c8c80afa7e4affdca54850bf0cc
MilesDev91 <34494229+MilesDev91@users.noreply.github.com>
2022-04-09 09:26:00 -0400

    Create guild form
    Mostly finished form. File input doesn't look great, but it works.

--- frontend/pages/guild/create.tsx
+++ frontend/pages/guild/create.tsx
@@ -1,16 +1,21 @@
 import { NextPage } from "next";
 import { useMoralis } from "react-moralis";
-import { Input } from "web3uikit";
+import { Button, Icon, Input } from "web3uikit";
 import useWindowWidth from "../../helpers/hooks/useWindowWidth";
+import styles from "../../styles/Guild.module.css";
 
 const CreateGuild: NextPage = () => {
   const { user } = useMoralis();
   const windowWidth = useWindowWidth(window);
 
+  const handleSubmit = () => {
+    console.log("hello");
+  };
+
   return (
     <div className="main">
       <h2>Create Guild</h2>
-      <form>
+      <form className={styles.formCreateGuild} onSubmit={handleSubmit}>
         <Input
           label="Guild name"
           name="Guild name"
@@ -28,6 +33,11 @@ const CreateGuild: NextPage = () => {
           type="number"
           width={windowWidth < 600 ? "80%" : "500px"}
         />
+        <label className={styles.labelFile} htmlFor="file">
+          Upload logo
+        </label>
+        <input id="file" className={styles.inputFile} type="file" />
+        <Button type="submit" text="Create" />
       </form>
     </div>
   );
