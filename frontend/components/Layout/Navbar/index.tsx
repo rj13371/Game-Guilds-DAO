27320ae12771148a0f738488b71f57ac70405f14
MilesDev91 <34494229+MilesDev91@users.noreply.github.com>
2022-04-09 04:17:11 -0400

    Wallet connection
    Added a moralis web3 server provider on _app wrapper.
    
    Added wallet connection to the navbar.

--- frontend/components/Layout/Navbar/index.tsx
+++ frontend/components/Layout/Navbar/index.tsx
@@ -1,9 +1,11 @@
 import styles from "./Navbar.module.css";
+import { ConnectButton } from "web3uikit";
 
 export default function Navbar(): JSX.Element {
   return (
     <nav className={styles.navBar}>
-      <h4>Game Guilds</h4>
+      <h4 className={styles.logo}>Game Guilds</h4>
+      <ConnectButton />
     </nav>
   );
 }
--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,12 +1,18 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Layout from "../components/Layout";
+import { MoralisProvider } from "react-moralis";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <MoralisProvider
+      serverUrl={process.env.NEXT_PUBLIC_SERVER_URL || ""}
+      appId={process.env.NEXT_PUBLIC_APP_ID || ""}
+    >
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </MoralisProvider>
   );
 }
 
